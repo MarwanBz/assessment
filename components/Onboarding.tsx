74124cf8d9e26a735775726695504a0f43e283ec
Marwan <kaka.mer1998@gmail.com>
2024-06-17 02:17:39 +0300

    feat(Onboarding): add chanage card dots  animation

--- components/Onboarding.tsx
+++ components/Onboarding.tsx
@@ -1,4 +1,14 @@
-import { FlatList, StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  Animated,
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  ViewToken,
+  useWindowDimensions,
+} from "react-native";
+import { useAnimatedRef, useAnimatedScrollHandler, useSharedValue } from "react-native-reanimated";
 
 import { OnboardingItem } from "./OnboardingItem";
 import { Pagination } from "./Pagination";
@@ -6,24 +16,45 @@ import React from "react";
 // import { Cards,type data }  from "@/data/cards";
 import { data } from "@/data/cards";
 
-// const Item = ({ title }) => (
-//   <View style={styles.item}>
-//     <Text style={styles.title}>{title}</Text>
-//   </View>
-// );
-
 export const Onboarding = () => {
+  const { width: SCREEN_WIDTH } = useWindowDimensions();
+  const flatListRef = useAnimatedRef<FlatList>();
+
+  const flatListIndex = useSharedValue(0);
+  const x = useSharedValue(0);
+
+  const onViewableItemsChanged = ({
+    viewableItems,
+  }: {
+    viewableItems: Array<ViewToken>;
+  }) => {
+    flatListIndex.value = viewableItems[0].index ?? 0;
+  };
+
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      x.value = event.contentOffset.x;
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <FlatList
+      <Animated.FlatList
+        ref={flatListRef as any}
         data={data}
-        renderItem={({ item }) => <OnboardingItem card={item} />}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item, index }) => (
+          <OnboardingItem   card={item} />
+        )}
+        onScroll={onScroll}
+        scrollEventThrottle={16}
         horizontal
         showsHorizontalScrollIndicator={false}
         bounces={false}
         pagingEnabled
+        onViewableItemsChanged={onViewableItemsChanged}
       />
-      <Pagination  />
+      <Pagination data={data} x={x} screenWidth={SCREEN_WIDTH} />
     </View>
   );
 };
@@ -31,5 +62,5 @@ export const Onboarding = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 0.8,
-  }
-})
+  },
+});
--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -7,29 +7,61 @@ import Animated, {
 import { StyleSheet, View } from "react-native";
 
 import React from "react";
-import { data, type Cards } from "@/data/cards";
+import { type Cards } from "@/data/cards";
 
-type PaginationCampProps = {
+type PaginationDotsProps = {
   index: number;
   x: SharedValue<number>;
   screenWidth: number;
-}
+};
 
-const PaginationComp = () => {
-  return <View style={styles.dots}></View>;
+const PaginationDots = ({ index, x, screenWidth }: PaginationDotsProps) => {
+  const animatedDotsStyle = useAnimatedStyle(() => {
+    const widthAnimation = interpolate(
+      x.value,
+      [
+        (index - 1) * screenWidth,
+        index * screenWidth,
+        (index + 1) * screenWidth,
+      ],
+      [10, 20, 10],
+      Extrapolate.CLAMP
+    );
+
+    const opacityAnimation = interpolate(
+      x.value,
+      [
+        (index - 1) * screenWidth,
+        index * screenWidth,
+        (index + 1) * screenWidth,
+      ],
+      [0.5, 1, 0.5],
+      Extrapolate.CLAMP
+    );
+    return {
+      width: widthAnimation,
+      opacity: opacityAnimation,
+    };
+  });
+  return <Animated.View style={[styles.dots, animatedDotsStyle]} />;
 };
 
 type PaginationProps = {
   data: Cards[];
   x: SharedValue<number>;
   screenWidth: number;
-}
+};
 
-export function Pagination() {
+export function Pagination({ data, screenWidth, x }: PaginationProps) {
   return (
     <View style={styles.container}>
       {data.map((item, index) => (
-        <PaginationComp key={item.id} />
+        <PaginationDots
+          key={item.id}
+          index={index}
+          x={x}
+          screenWidth={screenWidth}
+        />
       ))}
     </View>
   );
