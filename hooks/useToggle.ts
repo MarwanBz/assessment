20f492b83132b0f51f909b3ebb69a3325d4aedf7
Marwan <kaka.mer1998@gmail.com>
2024-06-25 10:59:46 +0300

    feat: create useToggle hook

---
+++ hooks/useToggle.ts
@@ -0,0 +1,11 @@
+import { boolean } from "zod";
+import { useState } from "react";
+export default function useToggle(defaultValue:boolean) {
+  const [value, setValue] = useState(defaultValue)
+  
+  function toggleValue(value:any) {
+    setValue(currentValue => typeof value === "boolean" ? value : !currentValue)
+  }
+
+  return [value,toggleValue]
+}
